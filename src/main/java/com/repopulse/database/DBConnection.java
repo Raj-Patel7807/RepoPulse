88b128ed0a9812c062c1a0e97808de80865b43d1
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-29 04:02:03 +0530

    feat(db): add JDBC connection manager

--- src/main/java/com/repopulse/database/DBConnection.java
+++ src/main/java/com/repopulse/database/DBConnection.java
@@ -1,4 +1,25 @@
 package com.repopulse.database;
 
+import com.repopulse.config.AppConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if(connection == null || connection.isClosed()) {
+                String url = AppConfig.get("db.url");
+                String username = AppConfig.get("db.username");
+                String password = AppConfig.get("db.password");
+
+                connection = DriverManager.getConnection(url, username, password);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("DB Connection failed", e);
+        }
+        return connection;
+    }
 }
