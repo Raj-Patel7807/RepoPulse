27c97aad15bdf47477888fd4e99e3d7a33d74cb0
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-29 04:04:20 +0530

    feat(cli): build CLI for auth

--- src/main/java/com/repopulse/cli/AuthCLI.java
+++ src/main/java/com/repopulse/cli/AuthCLI.java
@@ -1,4 +1,45 @@
 package com.repopulse.cli;
 
+import com.repopulse.model.User;
+import com.repopulse.service.AuthService;
+import com.repopulse.service.impl.AuthServiceImpl;
+import com.repopulse.util.Session;
+
+import java.util.Scanner;
+
 public class AuthCLI {
+    private Scanner sc = new Scanner(System.in);
+    private AuthService authService = new AuthServiceImpl();
+
+    public void login() {
+        System.out.print("Username: ");
+        String username = sc.next();
+
+        System.out.print("Password: ");
+        String password = sc.next();
+
+        User user = authService.login(username, password);
+
+        if(user != null) {
+            Session.setCurrentUser(user);
+            System.out.println("Login Successful...");
+        } else {
+            System.out.println("Invalid Credentials...");
+        }
+    }
+
+    public void signup() {
+        System.out.print("Enter Your Email: ");
+        String email = sc.next();
+
+        System.out.print("Choose username: ");
+        String username = sc.next();
+
+        System.out.print("Choose password: ");
+        String password = sc.next();
+
+        User newUser = new User(username, password, email);
+        authService.signup(newUser);
+        System.out.println("Signup Successful...");
+    }
 }
--- src/main/java/com/repopulse/cli/Menu.java
+++ src/main/java/com/repopulse/cli/Menu.java
@@ -1,4 +1,71 @@
 package com.repopulse.cli;
 
+import com.repopulse.cli.AuthCLI;
+import com.repopulse.util.Session;
+
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner sc = new Scanner(System.in);
+    private AuthCLI authCLI = new AuthCLI();
+
+    public void showAuthMenu() {
+        System.out.println("\n=== RepoPulse ===");
+        System.out.println("1. Login");
+        System.out.println("2. SignUp");
+        System.out.println("3. Exit");
+
+        System.out.print("Enter Choice: ");
+        int choice = sc.nextInt();
+
+        if(choice == 1) {
+            authCLI.login();
+        } else if(choice == 2) {
+            authCLI.signup();
+        } else if(choice == 3) {
+            System.out.println("Exiting....");
+            System.exit(0);
+        } else {
+            System.out.println("Invalid Choice...");
+        }
+    }
+
+    public void showUserMenu() {
+        System.out.println("\n=== Dashboard ===");
+        System.out.println("Welcome, " + Session.getCurrentUser().getUsername());
+
+        System.out.println("1. Create Repository");
+        System.out.println("2. View My Repositories");
+        System.out.println("3. Commit");
+        System.out.println("4. Logout");
+
+        int choice = sc.nextInt();
+
+        if(choice == 1) {
+            System.out.println("Create Repo - Not Implemented...");
+        } else if(choice == 2) {
+            System.out.println("View Repo - Not Implemented...");
+        } else if(choice == 3) {
+            System.out.println("Commit - Not Implemented...");
+        } else if(choice == 4) {
+            Session.logout();
+        } else {
+            System.out.println("Invalid Choice...");
+        }
+    }
+
+    private void logout() {
+        Session.logout();
+        System.out.println("Logged out successfully!");
+    }
+
+    public void start() {
+        while(true) {
+            if(Session.getCurrentUser() == null) {
+                showAuthMenu();
+            } else {
+                showUserMenu();
+            }
+        }
+    }
 }
