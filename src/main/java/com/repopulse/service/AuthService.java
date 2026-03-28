90a48761f64bdcbc3493f73d2f72aeeb8eb3396c
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-29 04:03:33 +0530

    feat(auth): implement login and signup service

--- src/main/java/com/repopulse/service/AuthService.java
+++ src/main/java/com/repopulse/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.repopulse.service;
 
+import com.repopulse.model.User;
+
 public interface AuthService {
+
+    User login(String username, String password);
+
+    void signup(User user);
 }
--- src/main/java/com/repopulse/service/impl/AuthServiceImpl.java
+++ src/main/java/com/repopulse/service/impl/AuthServiceImpl.java
@@ -1,4 +1,27 @@
 package com.repopulse.service.impl;
 
-public class AuthServiceImpl {
+import com.repopulse.model.User;
+import com.repopulse.service.AuthService;
+import com.repopulse.dao.UserDAO;
+import com.repopulse.dao.impl.UserDAOImpl;
+
+public class AuthServiceImpl implements AuthService {
+    private UserDAO userDAO = new UserDAOImpl();
+
+    @Override
+    public User login(String username, String password) {
+        User user = userDAO.getUserByUsername(username);
+        System.out.println("INPUT PASSWORD: [" + password + "]");
+        System.out.println("DB PASSWORD: [" + user.getPassword() + "]");
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+
+    @Override
+    public void signup(User user) {
+        userDAO.createUser(user);
+    }
 }
