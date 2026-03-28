4d34eca3eba0babc3226c1491fa5bf164a584c1d
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-29 04:03:11 +0530

    feat(dao): add user data access operations

--- src/main/java/com/repopulse/dao/UserDAO.java
+++ src/main/java/com/repopulse/dao/UserDAO.java
@@ -1,4 +1,9 @@
 package com.repopulse.dao;
 
+import com.repopulse.model.User;
+
 public interface UserDAO {
+    void createUser(User user);
+
+    User getUserByUsername(String username);
 }
--- src/main/java/com/repopulse/dao/impl/UserDAOImpl.java
+++ src/main/java/com/repopulse/dao/impl/UserDAOImpl.java
@@ -1,4 +1,49 @@
 package com.repopulse.dao.impl;
 
-public class UserDAOImpl {
+import com.repopulse.dao.UserDAO;
+import com.repopulse.database.DBConnection;
+import com.repopulse.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public void createUser(User user) {
+        String sql = "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)";
+
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        String sql = "SELECT * FROM users WHERE username = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(
+                        rs.getString("username"),
+                        rs.getString("password_hash"),
+                        rs.getString("email")
+                );
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
