7c4fee343b49f8b3edc3c786820e8ac2b8cdb348
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-30 05:01:39 +0530

    feat(model): upd models entities

--- src/main/java/com/repopulse/model/Repository.java
+++ src/main/java/com/repopulse/model/Repository.java
@@ -6,41 +6,65 @@ public class Repository {
     private String repoDesc;
     private long repoOwnerUserId;
     private String repoVisibilityType;
+    private Long defaultBranchId;
+    private Long parentRepositoryId;
+    private Long forkedFromCommitId;
 
     public Repository() {}
 
     public long getRepoId() {
-        return this.repoId;
+        return repoId;
     }
     public void setRepoId(long repoId) {
         this.repoId = repoId;
     }
 
     public String getRepoName() {
-        return this.repoName;
+        return repoName;
     }
     public void setRepoName(String repoName) {
         this.repoName = repoName;
     }
 
     public String getRepoDesc() {
-        return this.repoDesc;
+        return repoDesc;
     }
     public void setRepoDesc(String repoDesc) {
         this.repoDesc = repoDesc;
     }
 
     public long getRepoOwnerUserId() {
-        return this.repoOwnerUserId;
+        return repoOwnerUserId;
     }
     public void setRepoOwnerUserId(long repoOwnerUserId) {
         this.repoOwnerUserId = repoOwnerUserId;
     }
 
     public String getRepoVisibilityType() {
-        return this.repoVisibilityType;
+        return repoVisibilityType;
     }
     public void setRepoVisibilityType(String repoVisibilityType) {
         this.repoVisibilityType = repoVisibilityType;
+    }
+
+    public Long getDefaultBranchId() {
+        return defaultBranchId;
+    }
+    public void setDefaultBranchId(Long defaultBranchId) {
+        this.defaultBranchId = defaultBranchId;
+    }
+
+    public Long getParentRepositoryId() {
+        return parentRepositoryId;
+    }
+    public void setParentRepositoryId(Long parentRepositoryId) {
+        this.parentRepositoryId = parentRepositoryId;
+    }
+
+    public Long getForkedFromCommitId() {
+        return forkedFromCommitId;
+    }
+    public void setForkedFromCommitId(Long forkedFromCommitId) {
+        this.forkedFromCommitId = forkedFromCommitId;
     }
 }
--- src/main/java/com/repopulse/model/User.java
+++ src/main/java/com/repopulse/model/User.java
@@ -1,9 +1,18 @@
 package com.repopulse.model;
 
+import java.sql.Timestamp;
+
 public class User {
+    private long userId;
     private String username;
     private String password;
     private String email;
+    private String profileBio;
+    private String profileAvatarUrl;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private boolean isDeleted;
+    private Timestamp deletedAt;
 
     public User() {}
 
@@ -13,24 +22,80 @@ public class User {
         this.email = email;
     }
 
+    public User(long userId, String username, String password, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
     public String getUsername() {
-        return this.username;
+        return username;
     }
     public void setUsername(String username) {
         this.username = username;
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getProfileBio() {
+        return profileBio;
+    }
+    public void setProfileBio(String profileBio) {
+        this.profileBio = profileBio;
+    }
+
+    public String getProfileAvatarUrl() {
+        return profileAvatarUrl;
+    }
+    public void setProfileAvatarUrl(String profileAvatarUrl) {
+        this.profileAvatarUrl = profileAvatarUrl;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
     }
 }
