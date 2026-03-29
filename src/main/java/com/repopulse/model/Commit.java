9fff0dbdd7271b1aa12bc5401efee5e938855eba
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-29 06:19:25 +0530

    feat(model): add Commit and Repo entity

--- src/main/java/com/repopulse/model/Commit.java
+++ src/main/java/com/repopulse/model/Commit.java
@@ -1,4 +1,46 @@
 package com.repopulse.model;
 
 public class Commit {
+    private long commitId;
+    private long repoId;
+    private long authorUserId;
+    private Long parentCommitId;
+    private String commitMessage;
+
+    public Commit() {}
+
+    public long getCommitId() {
+        return this.commitId;
+    }
+    public void setCommitId(long commitId) {
+        this.commitId = commitId;
+    }
+
+    public long getRepoId() {
+        return this.repoId;
+    }
+    public void setRepoId(long repoId) {
+        this.repoId = repoId;
+    }
+
+    public long getAuthorUserId() {
+        return this.authorUserId;
+    }
+    public void setAuthorUserId(long authorUserId) {
+        this.authorUserId = authorUserId;
+    }
+
+    public Long getParentCommitId() {
+        return this.parentCommitId;
+    }
+    public void setParentCommitId(Long parentCommitId) {
+        this.parentCommitId = parentCommitId;
+    }
+
+    public String getCommitMessage() {
+        return this.commitMessage;
+    }
+    public void setCommitMessage(String commitMessage) {
+        this.commitMessage = commitMessage;
+    }
 }
--- src/main/java/com/repopulse/model/Repository.java
+++ src/main/java/com/repopulse/model/Repository.java
@@ -1,4 +1,46 @@
 package com.repopulse.model;
 
 public class Repository {
+    private long repoId;
+    private String repoName;
+    private String repoDesc;
+    private long repoOwnerUserId;
+    private String repoVisibilityType;
+
+    public Repository() {}
+
+    public long getRepoId() {
+        return this.repoId;
+    }
+    public void setRepoId(long repoId) {
+        this.repoId = repoId;
+    }
+
+    public String getRepoName() {
+        return this.repoName;
+    }
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+    public String getRepoDesc() {
+        return this.repoDesc;
+    }
+    public void setRepoDesc(String repoDesc) {
+        this.repoDesc = repoDesc;
+    }
+
+    public long getRepoOwnerUserId() {
+        return this.repoOwnerUserId;
+    }
+    public void setRepoOwnerUserId(long repoOwnerUserId) {
+        this.repoOwnerUserId = repoOwnerUserId;
+    }
+
+    public String getRepoVisibilityType() {
+        return this.repoVisibilityType;
+    }
+    public void setRepoVisibilityType(String repoVisibilityType) {
+        this.repoVisibilityType = repoVisibilityType;
+    }
 }
