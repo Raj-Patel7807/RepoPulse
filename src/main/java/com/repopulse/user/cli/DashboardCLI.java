0c6a364833977d96d8b0d83c9abe74c4e97c0258
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-04-14 22:39:07 +0530

    feat(user): add cli

--- src/main/java/com/repopulse/user/cli/DashboardCLI.java
+++ src/main/java/com/repopulse/user/cli/DashboardCLI.java
@@ -2,28 +2,37 @@ package com.repopulse.user.cli;
 
 import com.repopulse.common.cli.CliUtils;
 import com.repopulse.infra.session.Session;
+import com.repopulse.repository.cli.RepositoryCLI;
 import com.repopulse.user.model.User;
 import com.repopulse.user.service.UserService;
 
 public class DashboardCLI {
     private final UserService userService = new UserService();
 
+    private final RepositoryCLI repositoryCLI = new RepositoryCLI();
+
     public void start() {
         while(true) {
             System.out.println("\n=== Dashboard ===");
             System.out.println("Welcome, " + Session.getCurrentUser().getUsername());
-            System.out.println("1. Search User");
-            System.out.println("2. View Profile");
-            System.out.println("3. Logout");
+            System.out.println("1. My Profile");
+            System.out.println("2. My Repositories");
+            System.out.println("3. Search User");
+            System.out.println("4. Notifications");
+            System.out.println("5. Logout");
 
             int choice = CliUtils.getIntInput("Enter Choice: ");
 
             if(choice == 1) {
-                searchUser();
-            } else if(choice == 2) {
                 ProfileCLI profileCLI = new ProfileCLI(Session.getCurrentUser().getUsername(), false);
                 profileCLI.start();
+            } else if(choice == 2) {
+                repositoryCLI.start();
             } else if(choice == 3) {
+                searchUser();
+            } else if(choice == 4) {
+                System.out.println("Notification Pending........");
+            } else if(choice == 5) {
                 Session.setCurrentUser(null);
                 return;
             } else {
--- src/main/java/com/repopulse/user/cli/GuestCLI.java
+++ src/main/java/com/repopulse/user/cli/GuestCLI.java
@@ -1,10 +1,31 @@
 package com.repopulse.user.cli;
 
+import com.repopulse.common.cli.CliUtils;
+
 public class GuestCLI {
 
     public void start() {
-        while(true) {
 
+        while(true) {
+            System.out.println("\n=== Guest Menu ===");
+            System.out.println("1. Explore Repositories");
+            System.out.println("2. Search Users");
+            System.out.println("3. Back");
+
+            int choice = CliUtils.getIntInput("Enter Choice: ");
+
+            if(choice == 1) {
+                System.out.println("Explore Repo Pending.......");
+            } else if(choice == 2) {
+                String username = CliUtils.getStringInput("Enter Username: ");
+
+                ProfileCLI profileCLI = new ProfileCLI(username, false);
+                profileCLI.start();
+            } else if(choice == 3) {
+                return;
+            } else {
+                System.out.println("Invalid Choice!!");
+            }
         }
     }
 }
